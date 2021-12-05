e51dbd822abe0c42343ad252ab9195e698a5b8e7
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-05 17:43:43 +0200

    Last Update: 05.12.2021

--- FinalExam/test.py
+++ FinalExam/test.py
@@ -1,2 +1,20 @@
-example = None
-if example == None:
+import re
+text = input()
+user_list = []
+
+
+pattern = r'!([A-Za-z]+)!:\[([A-Za-z]+)\]'
+matches = re.finditer(pattern, text)
+if matches.group() is not None:
+    for match in matches:
+        command = match.group(2)
+        message = match.group(4)
+        
+    for _ in range (len(message)):
+        char = ord(message[_])
+        user_list.append(str(char))
+        
+    print(f"{command}: ")
+    print(" ".join(user_list))
+else:
+    print("The message is not valid")
